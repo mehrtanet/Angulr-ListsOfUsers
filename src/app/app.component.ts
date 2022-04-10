db57ce15a0305f34ebea6ae7b641fbaa173048ab
Fariba.Mshokoh <faribamohamadi.mr@gmail.com>
2022-04-10 20:52:40 +0430

    commit all first for list's of users

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { user } from './app.interface';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'test';
+  users: user[] = [
+    {
+    email:'a@a.con',
+    password:'123',
+    address: 'Kermanshah',
+      },
+
+    {
+      email:'b@b.con',
+      password:'456',
+      address: 'Tehran',
+        },
+      
+    {
+      email:'c@c.con',
+      password:'789',
+      address: 'Deutscland',
+        },
+    
+  ]
+
+  public onAdduser(email: string, password: string, address:string, e:Event){
+    e.preventDefault();
+    let u: user={
+      email: email,
+      password: password,
+      address: address,
+    }
+    
+    this.users.push(u);
+  }
+    
 }
